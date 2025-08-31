3500d7a9bb090cdb922d2b6c52db1ae8024a5c6a
DharaniTharan06 <duraisamydharanitharan@gmail.com>
2025-08-31 15:08:29 +0530

    Update: bg

--- src/App.jsx
+++ src/App.jsx
@@ -28,10 +28,7 @@ function App() {
   return (
     <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
-        style={{
-            backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
-        }}
-    >
+        style={{backgroundImage: `url('https://images.pexels.com/photos/730547/pexels-photo-730547.jpeg')`,}}>
         <div className="w-full">
             <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
                 <form
@@ -39,8 +36,7 @@ function App() {
                         e.preventDefault();
                         convert()
                        
-                    }}
-                >
+                    }}>
                     <div className="w-full mb-1">
                         <Inputbox
                             label="From"
@@ -55,8 +51,7 @@ function App() {
                         <button
                             type="button"
                             className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 px-2 py-0.5"
-                            onClick={swap}
-                        >
+                            onClick={swap}>
                             swap
                         </button>
                     </div>
